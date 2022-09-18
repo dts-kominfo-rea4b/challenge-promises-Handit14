b12c662aa815e16fc87472ed6df8b004ac8c6aa3
Handit14 <farhanaditwork14@gmail.com>
2022-09-18 14:17:37 +0700

    menambahkan isi promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const firstStep = await promiseTheaterIXX();
+    const secondStep = await promiseTheaterVGC();
+    const allStep = [...firstStep, ...secondStep].filter((value) => value.hasil === emosi).length;
+    return allStep;
+  } catch (error) {
+    console.log(error)
+  }
+} 
 
 module.exports = {
   promiseOutput,
